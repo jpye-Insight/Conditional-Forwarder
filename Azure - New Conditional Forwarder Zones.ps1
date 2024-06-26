1d8f76d0885b1cb50aa0bbb7c17a14c096a620c9
jpye-Insight <148391001+jpye-Insight@users.noreply.github.com>
2024-06-26 09:30:12 +0100

    Add files via upload

---
+++ Azure - New Conditional Forwarder Zones.ps1
@@ -0,0 +1,27 @@
+﻿Write-host @"
+Starting script to create New Conditional Forwarder zones in DNS servers.
+
+"@
+
+$MasterServer = "ADDIPS"
+$DNSzones = import-csv c:\temp\AzureprivateDnsZones.csv
+
+
+foreach($PrivateDNSzone in $DNSzones){
+    
+    if($PrivateDNSzone.NAME -like "privatelink.*"){
+        $PrivateDNSzone.NAME = $PrivateDNSzone.NAME.substring($("privatelink.").Length)
+    }
+
+    write-host "Creating new Conditional Forwarder zone for"$PrivateDNSzone.NAME
+    Add-DnsServerConditionalForwarderZone -Name $PrivateDNSzone.NAME -ReplicationScope "Forest" -MasterServers $MasterServer
+    Add-DnsServerConditionalForwarderZone -Name $PrivateDNSzone.NAME -MasterServers "168.63.129.16"
+
+}
+
+write-host @"
+    
+    Ending script. 
+
+"@
+write-host "New Conditional Forwarder zones created for"$DNSzones.count"Zones"
